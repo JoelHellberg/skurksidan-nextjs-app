ee554ab672f8142ee7a4b360420e72913b7bccab
Joel Hellberg <joehe737@student.liu.se>
2024-08-18 20:34:05 +0200

    Rätt storys

--- data/character_data.tsx
+++ data/character_data.tsx
@@ -5,17 +5,20 @@ export const characters =
     {
       name: "Bree",
       surname: "C. Zeyl",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Bree och hennes tvillingsyster föddes in i en högt ansedd familj. De var oskiljaktiga och kända som de busigaste av alla barn, vilket snart väckte en växande avsky hos resten av familjen. Tvillingarna tvingades ofta sitta inlåsta i sina rum efter sina hyss." +
+      "\nEn mörk och kall natt stod familjens stora villa plötsligt i lågor. Ingenting kunde räddas, och i ruinerna hittades endast Wendy Ceglith och Bree C. Zeyl. Rykten spreds snart i byn om att det var tvillingarna som orsakat branden. Som straff blev de förvisade och tvingades ut på havet i en gammal eka. Sedan dess har ingen sett dem igen." +
+      "\nSägnen säger att de nu seglar med Captain Jill S. Creu på det ökända skeppet Mörkrets Vrede. I mörkrets famn har Bree och hennes syster funnit sin plats som båtsmän ombord, där de styr arbetslagen med hårda metoder. Skeppets skuggsida har fått två nya härskare."
     },
     {
       name: "Connie",
       surname: "Trast Valoo",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Connie Trast Valoo växte upp i ett land långt bort från havet på en bondgård. Efter en tragisk händelse förlorade Connie sin enda syster, efter denna händelse valde hon att inte längre söka ljuset utan acceptera den mörka ondskan. Därefter drogs hon mot havets äventyr. Hon förflyttade sig i skeppens skuggor för att stjäla från rika affärsmän. Hennes rörelser skedde i fullständig tystnad och liknande dimma över vattenytan. Rykten spreds om en tjuv som aldrig syntes eller hördes, det enda beviset om tjuvens existens var de stulna varorna. Detta rykte nådde tillslut Captain Jill S. Creu, som rekryterade Connie till det mäktiga skeppet Mörkrets Vrede. På skeppet är hon känd som den otåliga då hon vill förbli i konstant rörelse mot besättningens mål. Connie’s starka koppling till mörkret gör henne till en farlig motståndare."
     },
     {
       name: "Maddy",
       surname: "Cash",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Maddy Cash växte upp i en hamnstad, där hon redan som barn lärde sig att överleva på gatorna genom att stjäla från rika handelsresande. Med tiden blev hon en skicklig tjuv med rykte om sig att vara oslagbar. En natt lyckades Maddy stjäla från ett välbevakad handelsfartyg, vilket skapade stor uppståndelse i hamnen." +
+      "\nHennes djärvhet fångade intresset hos Captain Jill S. Creu. När kaptenen fick reda på Maddys bravader och insåg hennes potential, bestämde hon sig för att söka upp den unga tjuven. Imponerad av Maddys mod och färdigheter, erbjöd Captain Jill S. Creu henne en plats i besättningen som kvartermästare. Maddy, som längtade efter äventyr och en chans att få använda sina färdigheter till något större, accepterade erbjudandet och blev en värdefull medlem av besättningen."
     },
     {
       name: "Jaques",
@@ -29,37 +32,41 @@ export const characters =
       surname: "S. Creu",
       story: "Jill S. Creu föddes en mycket stormig och mörk natt. Hon växte upp som säljare i en fiskeby och visade tidigt en skoningslös beslutsamhet som vann respekt och fruktan från piraterna på genomresa i hamnstaden. Efter att hon en natt mött Tengil var hennes mål tydligt: att bli kapten för det mest fruktade skeppet på de sju haven." + 
       "\nHennes rykte som kapten spred sig som en löpeld och resan för att samla världens bästa skurkar till sin besättning var lång. Hon sökte upp legendariska tjuvar i skuggiga bakgator, mutade besättningar på andra skepp och vann över livrädda sjömän med sina övertygande ord. Hon visste att för att kunna erövra ljuset och bli en del av mörkret på haven behövde hon en besättning som var skrämmande och skoningslös." +
-      "\nCaptain Jill S. Creu och hennes besättning blev en legend som fruktades av alla på de sju haven. Med hennes besättning av skurkar vid sin sida fortsatte hon att segla mot sina mål, alltid i mörkret och alltid med Tengils aaaahhhhhh"
+      "\nCaptain Jill S. Creu och hennes besättning blev en legend som fruktades av alla på de sju haven. Med hennes besättning av skurkar vid sin sida fortsatte hon att segla mot deras mål."
     },
     {
       name: "Sir Von",
       surname: "D. Fleet",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Sir von D. Fleet, en före detta betjänt av det kungliga hovet, levde ett liv präglat av etikett och disciplin med hängivenhet till sin tjänst. Men ödet tog en oväntad vändning när han en dag blev kidnappad av en fruktad piratbesättning. Hans aristokratiska bakgrund gjorde honom först till en udda figur, men hans naturliga gästfrihet gjorde att han snabbt vann besättningens respekt. Trots att piratlivet var en värld bort från det kungliga hovet, anpassade han sig fort till det nya piratlivet. Sir von D. Fleet fann nämligen något oväntat: en känsla av frihet och äventyr som han aldrig tidigare upplevt."
     },
     {
       name: "Sav Y. May",
       surname: "Thea",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Sav Y. May Thea föddes i en lågt bemedlad familj på en liten folkrik ö mitt i det mörka havet. Trots hennes föräldrars desperata ansträngningar att forma henne till en god och rättfärdig varelse, blommade en skugga av ondska i hennes själ. Som ett sista hopp att rädda familjens förlorade lycka, drevs de av skräck till att kasta ut henne, denna flicka vars handlingar speglade ondskan själv, ut i den hårda världen. När familjen sedan mystiskt försvann, flydde hon ut mot det okända havet. Hon hoppade från skepp till skepp och sökte skatter över hela världen. Rykten om hennes lyckosamma äventyr spred sig som en viskning i vinden, och nådde till slut öronen på Captain Jill S. Creu, en härskare över mörkrets krafter. Fascinerad av kaptenens obändiga vilja och beslutsamhet att låta mörkret regera, fann Sav Y. May Thea sin plats. Hon valde att förena sitt öde med Mörkrets Vrede och blev en del av besättningen, en odödlig skugga bland nattsvarta vågor."
     },
     {
       name: "Wendy",
       surname: "Ceglith",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Wendy och hennes tvilling föddes in i en högt ansedd familj. Dem två tvillingarna var de busigaste av alla barn, detta byggde upp ett avsky mot dem från övriga familjen. De blev ofta inlåsta i sina rum efter hyss. En natt efter många år stod plötsligt den stora villan i lågor. Kvar i ruinerna var endast Wendy Ceglith och Bree C. Zeyl. I byn spreds berättelser om att tvillingarna låg bakom den ökända branden. Därefter tvingade byn ut tvillingarna ut på det öppna havet i en gammal eka och dem syntes aldrig igen. Sägnen säger att de kan hittas på Captain Jill S. Creu skepp drivet av mörker."
     },
     {
       name: "Lincoln",
       surname: "Bayou",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Lincoln Bayou växte upp på en gammal herrgård, där han tidigt fick lära sig livets hårda lagar. Efter att hans far, en respekterad man som arbetat hela sitt liv i högsta domstol, hårt kritiserat och hatat Lincoln när han växt upp, flydde Lincoln från sin far. Fylld av sorg och hämndlystnad, begav sig Lincoln ut på de mörka haven för finna en förmögenhet för att bli accepterad av sin far. Under sin resa blev han härdad av våld och motgångar, och hans hjärta mörknade. När han till slut konfronteras av Captain Jill S. Creu, såg hon potential i honom. Istället för att döda honom, rekryterade hon honom till besättningen som profoss, ansvarig för att upprätthålla disciplin med en järnhand." +
+      "\nDriven av mörker och hat mot sin far som aldrig såg honom som en son. Han blev snabbt fruktad, en man som fann sin makt i mörkret han en gång avskydde. Nu är han en del av Mörkret, en skugga som vaktar besättningens ordning och döljer sina egna demoner under ytan."
     },
     {
       name: "Noa R.",
       surname: "Steass",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Noa R. Steass växte upp i den trygga skuggan av välstånd, en arvtagare till en förmögen familj där bekymmer var få. Men till Noas familjs stora missnöje var han bästa vän med byns rebell och ligist Jill S. Creu. Deras band var likt knutna rep, starka och oövervinneliga. Som ungdom lockades han av hennes äventyrliga ådra och följde henne genom alla farvatten och faror." +
+      "\nTill slut hade Noas föräldrar fått nog av att se deras sons liv gå mot ett liv av illdåd och farligheter. De ställde ett Noa inför ett val, bryta all kontakt med sin bästa vän eller aldrig sätta sin fot i deras hem igen. Sen den dagen har han varit vid hennes sida som hennes trogna högra hand, Captain Jill S. Creus andrekapten. Hans öra är alltid på vakt för de rykten som strömmar genom luften och havet, för han vet att kunskap är nyckeln till överlevnad och framgång i den oroliga världen på havet."
     },
     {
       name: "T. Rolph",
       surname: "Ace",
-      story: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      story: "Det var en gång en ung pojke vid namn Rolph, som levde ett stillsamt liv med sin familj. En dag gav de sig ut på en resa över det stora havet, men deras skepp sveptes snart in i ett ogenomträngligt mörker och blev plötsligt belägrat av fiender. I den desperata striden gömde Rolphs mor honom i en tunna, och det var det sista han mindes innan allt blev svart." + 
+      "\nNär Rolph vaknade fann han sig på ett främmande skepp och fylld av vrede rusade han ut ur tunnan. Men istället för att möta sin undergång, omringades han av skuggiga gestalter. Kaptenen för detta mystiska skepp, en kvinna vid namn Jill S. Creu, såg mörkret i honom." +
+      "\nIstället för att förgöra honom, lät kapten Jill honom ansluta till besättningen. Från den dagen blev Rolph en del av en mörk och farlig skara, och hans liv tog en ny och ödesdiger vändning."
     },
   ]
 };
