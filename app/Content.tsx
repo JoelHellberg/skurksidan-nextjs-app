16bb064298be4c0c4f7abc10a11e71631915b15b
Joel Hellberg <joehe737@student.liu.se>
2024-08-18 14:43:53 +0200

    Lagt till Voyado som sponsor
    Samt ändrat till rätt logga

--- app/Content.tsx
+++ app/Content.tsx
@@ -229,6 +229,7 @@ const Content = (props: Props) => {
       mobile:px-8   
       ">
         <Sponsor company1="ICA" company2="" className="grow-1 h-52 w-1/3 mobile:w-3/5" />
+        <Sponsor company1="Voyado" company2="" className="grow-1 h-48 w-1/3 mobile:w-4/5" />
         {/*<Sponsor company1="NAB" company2="Voyado" className="grow-1 h-48 w-1/3 mobile:w-3/5" />*/}
         {/*<Sponsor company1="Micropower" company2="" className="grow-2 h-48 w-2/3 mobile:w-full" />*/}
       </div>
