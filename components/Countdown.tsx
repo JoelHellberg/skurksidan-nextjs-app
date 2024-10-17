4d1dacd5077f46e48b7e60cb5677ba7e4e661ec0
Joel Hellberg <joehe737@student.liu.se>
2024-10-18 00:25:50 +0200

    Datumet behövde ändras på ett ställe till

--- components/Countdown.tsx
+++ components/Countdown.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react'
 import Image from 'next/image';
 
 const Countdown = () => {
-  const countDownDate = new Date("Oct 18 2024 00:00:00").getTime();
+  const countDownDate = new Date("Oct 20 2024 00:00:00").getTime();
 
   const [timeLeft, setTimeLeft] = useState({ d: "0", h: "00", m: "00", s: "00" })
 
