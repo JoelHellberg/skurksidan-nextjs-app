e8f7e5aa4681d984c94b6f77a39d3207395a5f4a
Joel Hellberg <joehe737@student.liu.se>
2024-11-19 21:25:17 +0100

    Commit för att byta deployment

--- app/Content.tsx
+++ app/Content.tsx
@@ -23,7 +23,7 @@ type Props = {
 const Content = (props: Props) => {
   function handleClick() { props.openGameRules() }
   const isHandheld = useMediaQuery('(max-width: 1050px)')
-  // Conditionally set the height
+  // Conditionally set the height 
   const divStyle = {
     backgroundImage: 'url(/images/paperTest.png)',
     backgroundSize: '100% 100%',
